e86ab9dc869751da300e051d28a5ee854ce194ee
Hermann-Josef Winter <hjw2803@googlemail.com>
2024-03-23 21:45:12 +0100

    update virtual environment

--- django_project/settings.py
+++ django_project/settings.py
@@ -1,5 +1,3 @@
-import os
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -9,10 +7,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
-DEBUG = os.environ.get("DJANGO_DEBUG", default=False)
+SECRET_KEY = "33o#*w(+)@e@sktjs8lb#36g17^0jir(mzl6zg9@4h86#9_-"
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
